2247a88293f04ad9c4e23f1a1e1845efb3d76e75
claraisley <isleyc97@gmail.com>
2019-12-17 23:44:56 +0000

    Added the structure and test data for the render function, and added comments for code clarity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,6 +18,7 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
+//Function that ensures that a user cannot put javascript in their tweet to hack someone else's browser, for security reasons.
 
 function escape(str) {
   var div = document.createElement('div');
@@ -25,26 +26,26 @@ function escape(str) {
   return div.innerHTML;
 }
 
+//Uses monent to return date in relation to the time now, eg. a few seconds ago, or 2 minutes ago
+
 function createdAt(time) {
-
-return moment(time).fromNow();
-
+  return moment(time).fromNow();
 }
 
 
+//Takes tweet JSON object and generates the DOM structure for a tweet, based on that object
 
-
-const createTweetElement = function(tweetData) {
+const createTweetElement = function(tweet) {
 
   let markup = $(`
   <article class="tweets">
   <header>
-    <span id="username">${escape(tweetData.user.name)}</span>
-    <span id="userhandle">${escape(tweetData.user.handle)}</span>
+    <span id="username">${escape(tweet.user.name)}</span>
+    <span id="userhandle">${escape(tweet.user.handle)}</span>
   </header>
-    <p class="tweet-text"> ${escape(tweetData.content.text)}</p> 
+    <p class="tweet-text"> ${escape(tweet.content.text)}</p> 
   <footer>
-    <p id="tweet-age">${createdAt(tweetData.created_at)}</p>
+    <p id="tweet-age">${createdAt(tweet.created_at)}</p>
   </footer>
 </article>  
 `)
@@ -54,22 +55,51 @@ return $markup;
 
 
 
-
-
-
-
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+//Loops through the array of tweet objects and appends each one to the "tweets-container"
+
+const renderTweets = function(tweets) {
+
+  for
 }
+
+
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
 
 const $tweet = createTweetElement(tweetData);
   console.log($tweet);
