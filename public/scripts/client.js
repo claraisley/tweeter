643b4f85baef0dc0d24cdb479046ca9e789af20b
claraisley <isleyc97@gmail.com>
2019-12-18 00:01:59 +0000

    Added logic that loops through the array of tweet objects, and uses the createTweetElement function to append each tweet to the body of the webpage. It renders the tweets when the document is ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const tweet = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// Test / driver code (temporary). Eventually will get this from the server.
 
 //Function that ensures that a user cannot put javascript in their tweet to hack someone else's browser, for security reasons.
 
@@ -32,6 +19,16 @@ function createdAt(time) {
   return moment(time).fromNow();
 }
 
+//Loops through the array of tweet objects and appends each one to the "tweets-container"
+
+const renderTweets = function(tweets) {
+
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+  return $tweet;
+}
 
 //Takes tweet JSON object and generates the DOM structure for a tweet, based on that object
 
@@ -52,7 +49,6 @@ const createTweetElement = function(tweet) {
 let $markup = $(markup);
 return $markup;
 }
-
 
 
 // Fake data taken from initial-tweets.json
@@ -81,31 +77,11 @@ const data = [
   }
 ]
 
-//Loops through the array of tweet objects and appends each one to the "tweets-container"
-
-const renderTweets = function(tweets) {
-
-  for
-}
 
 
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+//Renders tweets when the document is ready
 
-const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-// Test / driver code (temporary)
- // to see what it looks like
 $(document).ready(function() {
-$('#tweets-container').append($tweet);
-})
- // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+});
+
